e880cf0915c34328d007e31959eac160c1426651
adetoyea28 <142647086+adetoyea28@users.noreply.github.com>
2025-01-05 09:07:24 -0800

    Update userController.js

--- models/userController.js
+++ models/userController.js
@@ -1,3 +1,5 @@
 import User from './user.js';
 
-export const createUser = ()
+export const createUser = (username, email, password, favoriteClub, role, profilePic)=> {
+  const user = new User({username, email, password, favoriteClub, role, profilePic})
+}

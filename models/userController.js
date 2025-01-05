7451be6a34c6a57e483de9b102669a5acd8a1015
adetoyea28 <142647086+adetoyea28@users.noreply.github.com>
2025-01-05 09:59:17 -0800

    Update userController.js

--- models/userController.js
+++ models/userController.js
@@ -1,21 +1,52 @@
 import User from './user.js';
+import bycrypt from 'bycryptjs';
+
+export const createUser = (username, email, password, googleId, favoriteClub, trophies, fanscore, oauth, role, profilePic, stats)=> {
+  bycrypt.genSalt(10, (err, salt)=>{
+    bycrypt.hash(password, Salt, (err, hash)=>{
+      if(err){
+        return `error: ${err}`
+      }
+      hashedPassword = hash;
+      const user = new User({username, email, password, googleId, favoriteClub, trophies, fanscore, oauth, role, profilePic, stats});
+      const result = user.save();
+      return result;
+         }
+  })
+};
+
 
 export const findUser = (username, email, password, googleId)=> {
-  const result = User.findOne({username, email, password, googleId});
-  return result;
+  bycrypt.genSalt(10, (err, salt)=>{
+    bycrypt.hash(password, Salt, (err, hash)=>{
+      if(err){
+        return `error: ${err}`
+      }
+      hashedPassword = hash;
+      const result = User.findOne({username, email, hashedPassword, googleId});
+      return result;
+    }
+  })
 };
 
 export const updateUser = (username, email, password, favoriteClub, role, profilePic)=> {
-  const result = User.UpdateOne({ username, email, password },
+  bycrypt.genSalt(10, (err, salt)=>{
+    bycrypt.hash(password, Salt, (err, hash)=>{
+      if(err){
+        return `error: ${err}`
+      }
+      hashedPassword = hash
+      const result = User.UpdateOne({ username, email, hashedPassword },
                                 {$set: {username: username, 
-                                        password: password,
+                                        password: hashedPassword,
                                         favoriteClub: favoriteClub,
                                         role: role,
                                         profilePic: profilePic}
                                 });
-  return result;
+      return result;
+    }
+  })
 };
-
 export const deleteUser = (username, email, password, googleId, favoriteClub, trophies, fanscore, oauth, role, profilePic, stats)=> {
   const result = User.deleteOne({username, email, password, googleId, favoriteClub, trophies, fanscore, oauth, role, profilePic, stats});
   return result;

73b6c3529c886c00466e90be56c52d95ee086978
adetoyea28 <142647086+adetoyea28@users.noreply.github.com>
2025-01-05 09:34:14 -0800

    Update userController.js

--- models/userController.js
+++ models/userController.js
@@ -1,5 +1,28 @@
 import User from './user.js';
 
-export const createUser = (username, email, password, favoriteClub, role, profilePic)=> {
-  const user = new User({username, email, password, favoriteClub, role, profilePic})
-}
+export const createUser = (username, email, password, googleId, favoriteClub, trophies, fanscore, oauth, role, profilePic, stats)=> {
+  const user = new User({username, email, password, googleId, favoriteClub, trophies, fanscore, oauth, role, profilePic, stats});
+  const result = user.save();
+  return result;
+};
+
+export const findUser = (username, email, password, googleId)=> {
+  const result = User.findOne({username, email, password, googleId});
+  return result;
+};
+
+export const updateUser = (username, email, password, favoriteClub, role, profilePic)=> {
+  const result = User.UpdateOne({ username, email, password },
+                                {$set: {username: username, 
+                                        password: password,
+                                        favoriteClub: favoriteClub,
+                                        role: role,
+                                        profilePic: profilePic}
+                                });
+  return result;
+};
+
+export const deleteUser = (username, email, password, googleId, favoriteClub, trophies, fanscore, oauth, role, profilePic, stats)=> {
+  const result = User.deleteOne({username, email, password, googleId, favoriteClub, trophies, fanscore, oauth, role, profilePic, stats});
+  return result;
+};
